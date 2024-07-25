5adf9236204af0e7f1544402771f5fb081c23011
lancer-code <xeroclson75@gmail.com>
2024-07-25 16:59:17 +0500

    Add Mongoose Modal and Schemas

---
+++ src/app/models/Users.ts
@@ -0,0 +1,81 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isAcceptingMessage: boolean;
+  isVerifed: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "Username is Required"],
+    trim: true,
+    minlength: 4,
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email is Required"],
+    trim: true,
+    match: [/.+\@.+\..+/, "Enter a valide Email"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Enter Password"],
+  },
+
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is Required"],
+  },
+
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Exipry is Required"],
+  },
+
+  isVerifed: {
+    type: Boolean,
+    default: false,
+  },
+
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.model.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
